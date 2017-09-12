7bab415413859f3de1502e4a3b5d8025048d6615
D7703_17 <d7703_17@D7703-17ui-iMac.local>
2017-09-12 14:46:16 +0900

    merge

--- 03_Git_Branch_Test/ViewController.swift
+++ 03_Git_Branch_Test/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
             // Do any additional setup after loading the view, typically from a nib.
             print("Initial commit")
             print("deveplopre ")
+            print("Merge")
       }
 
       override func didReceiveMemoryWarning() {
